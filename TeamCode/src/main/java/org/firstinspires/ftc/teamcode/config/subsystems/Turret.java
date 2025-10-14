a09d4fc97e1f1d22246cf5368a0fe6a55feb77ea
Genius0412 <genius0412@naver.com>
2025-10-14 19:54:17 -0400

    Turret find gear ratio

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/subsystems/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/subsystems/Turret.java
@@ -11,7 +11,12 @@ import org.firstinspires.ftc.teamcode.config.core.SubsysCore;
 public class Turret extends SubsysCore {
     CRServo s1, s2;
     AbsoluteAnalogEncoder e1, e2;
+
     int fw;
+    final double GEAR_RATIO = (double) 180 /50 * 20 / 90;
+
+    public double servoToTurretDegrees(){  }
+    public double turretToServoDegrees(){ }
 
     public Turret(){
         e1 = new AbsoluteAnalogEncoder(h, "te1", 3.3, AngleUnit.DEGREES);
